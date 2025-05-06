25887625c87d8e032215988ee145e5ce5089bd03
Kenlark <kenzokerachi@hotmail.fr>
2025-05-06 11:59:13 +0200

    feat : ajout d'un treemap pour ajouter, supprimer et trier via leurs clés respectives

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Main {
     public static void main(String[] args) {
 
-
+        Map<String , Integer> ages = new TreeMap<>();
+        ages.put( "Paul", 45 );
+        ages.put( "Jane", 26 );
+        ages.put( "Alban", 55 );
+        System.out.println(ages);
+        ages.remove("John");
+        System.out.println(ages);
 
     }
 }
